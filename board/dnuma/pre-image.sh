d0c0a17aac4808a110b34fead17de4c48e4137de
Cody P Schafer <cody@linux.vnet.ibm.com>
2012-10-04 12:59:30 -0700

    dnuma: login hacks.

--- board/dnuma/pre-image.sh
+++ board/dnuma/pre-image.sh
@@ -2,4 +2,5 @@
 
 TARGET_DIR="$1"
 
+sed -i 's~ttyS0::respawn:/sbin/getty -L ttyS0 115200 vt100 # GENERIC_SERIAL~ttyS0::respawn:/bin/login -f root # GENERIC_SERIAL~' ${TARGET_DIR}/etc/inittab
 echo "debugfs		/sys/kernel/debug	       debugfs    defaults	  0	 0" >>"${TARGET_DIR}/etc/fstab"
