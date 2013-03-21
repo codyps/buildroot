87338c805ecbeef9fb69a41fd5cbf382342696da
Cody P Schafer <jmesmon@gmail.com>
2013-03-21 02:27:19 -0700

    cryptsetup: initial cut.

---
+++ package/cryptsetup/cryptsetup.mk
@@ -0,0 +1,36 @@
+#############################################################
+#
+# cryptsetup
+#
+#############################################################
+
+CRYPTSETUP_VERSION = v1_5_1
+CRYPTSETUP_SITE = https://code.google.com/p/cryptsetup/
+CRYPTSETUP_SITE_METHOD = git
+CRYPTSETUP_LICENSE = GPLv2+
+CRYPTSETUP_LICENSE_FILES = COPYING
+
+## Prefer full-blown wget over busybox
+#ifeq ($(BR2_PACKAGE_BUSYBOX),y)
+#	WGET_DEPENDENCIES += busybox
+#endif
+#
+#ifeq ($(BR2_PACKAGE_GNUTLS),y)
+#	WGET_CONF_OPT += --with-ssl=gnutls \
+#		--with-libgnutls-prefix=$(STAGING_DIR)
+#	WGET_DEPENDENCIES += gnutls
+#endif
+#
+#ifeq ($(BR2_PACKAGE_OPENSSL),y)
+#	WGET_CONF_OPT += --with-ssl=openssl --with-libssl-prefix=$(STAGING_DIR)
+#	WGET_DEPENDENCIES += openssl
+#endif
+#
+## --with-ssl is default
+#ifneq ($(BR2_PACKAGE_GNUTLS),y)
+#ifneq ($(BR2_PACKAGE_OPENSSL),y)
+#	WGET_CONF_OPT += --without-ssl
+#endif
+#endif
+
+$(eval $(autotools-package))
