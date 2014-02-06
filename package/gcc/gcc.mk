b93af0de762e93b53918c4524587df6e6cb00f3f
Cody P Schafer <cody@linux.vnet.ibm.com>
2014-02-06 15:42:15 -0800

    multilib is hard

--- package/gcc/gcc.mk
+++ package/gcc/gcc.mk
@@ -101,6 +101,7 @@ HOST_GCC_COMMON_CONF_OPT = \
 	--disable-__cxa_atexit \
 	--with-gnu-ld \
 	--disable-libssp \
+	--disable-multilib \
 	--with-gmp=$(HOST_DIR)/usr \
 	--with-mpfr=$(HOST_DIR)/usr
 
